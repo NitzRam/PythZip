8084a87620a735395908f7592dfd9d3ab31dd54c
NitzRam <nithyaramanath@gmail.com>
2016-11-09 16:29:24 +0530

    Added comments

--- helloworld.py
+++ helloworld.py
@@ -3,14 +3,14 @@
 import os
 import shutil
 
-source_path = "/home/nithya/sorc"
-source_folder_content = os.listdir(source_path)
+source_path = "/home/nithya/sorc" #Assigning path of the source folder
+source_folder_content = os.listdir(source_path) #Assigning list of files and directories in source folder
 
-for file in source_folder_content:
+for file in source_folder_content: #Prints the list of files and directories
 	print file
 
-destination_path = "/home/nithya/dest"
-destination_folder_content = os.listdir(destination_path)
+destination_path = "/home/nithya/dest"  #Assigning path of the destination folder
+destination_folder_content = os.listdir(destination_path)#Assigning list of files and directories in destination folder
 
 
 
@@ -19,14 +19,14 @@ list = []
 
 
 
-def convert_bytes(num):
+def convert_bytes(num):#Function to convert bytes 
 	for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
 		if num < 1024.0:
 			return "%3.1f %s" % (num, x)
 		num /= 1024.0
 
 
-def file_size(element):
+def file_size(element):#Function to find size of file
 	if os.path.isfile(element):
 		file_info = os.stat(element)
 		return file_info.st_size
@@ -40,18 +40,18 @@ i = 0
 new_size = 0
 limit = 4000000
 for file in source_folder_content:
-	filename = os.path.join(source_path, file)
+	filename = os.path.join(source_path, file)#To obtain complete path
 	if os.path.isfile(filename):
-		file_list.append(filename)
+		file_list.append(filename)#Appending the file to the list file_list
 		size = file_size(filename)
 		print size
-		new_size = new_size + size
+		new_size = new_size + size #Adding size of each file
 		print "new_size:",new_size
-		if new_size < limit:
-			new_path = destination_path + "/" + "test" + str(i)
+		if new_size < limit: #If the total size is less than the limit copy the files to test+str(i) 
+			new_path = destination_path + "/" + "test" + str(i)#Creating new path
 			print new_path
 			print "new new_size",new_size
-			shutil.copy(filename, new_path)
-		if new_size > limit:
+			shutil.copy(filename, new_path)#To copy files
+		if new_size > limit:#If the total size greater than limit reset new_size
 		 	new_size=0;i = i + 1
 
